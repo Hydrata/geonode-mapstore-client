e2ff0eabead1893399e1d13d937923b6a1a0d310
davekennewell <davekennewell@gmail.com>
2021-07-25 17:20:14 +0400

    more effective parsing of variables for the graph

--- geonode_mapstore_client/client/js/plugins/hydrata/Biocollect/actionsBiocollect.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Biocollect/actionsBiocollect.js
@@ -1,6 +1,8 @@
 const SET_VISIBLE_BIOCOLLECT_CHART = 'SET_VISIBLE_BIOCOLLECT_CHART';
 const SAVE_SWAMPS_SURVEY_QUERY_TO_STORE = 'SAVE_SWAMPS_SURVEY_QUERY_TO_STORE';
 const SET_CURRENT_BIOCOLLECT_SURVEY_SITE_ID = 'SET_CURRENT_BIOCOLLECT_SURVEY_SITE_ID';
+const SET_SELECTED_X_KEY = 'SET_SELECTED_X_KEY';
+const SET_SELECTED_Y_KEY = 'SET_SELECTED_Y_KEY';
 
 function setVisibleBiocollectChart(visible) {
     return {
@@ -23,8 +25,24 @@ function saveSwampsQueryToStore(swampsSurveyData) {
     };
 }
 
+function setSelectedXKey(xKey) {
+    return {
+        type: SET_SELECTED_X_KEY,
+        selectedXKey: xKey
+    };
+}
+
+function setSelectedYKey(yKey) {
+    return {
+        type: SET_SELECTED_Y_KEY,
+        selectedYKey: yKey
+    };
+}
+
 module.exports = {
     SET_VISIBLE_BIOCOLLECT_CHART, setVisibleBiocollectChart,
     SET_CURRENT_BIOCOLLECT_SURVEY_SITE_ID, setCurrentBiocollectSurveySiteId,
-    SAVE_SWAMPS_SURVEY_QUERY_TO_STORE, saveSwampsQueryToStore
+    SAVE_SWAMPS_SURVEY_QUERY_TO_STORE, saveSwampsQueryToStore,
+    SET_SELECTED_X_KEY, setSelectedXKey,
+    SET_SELECTED_Y_KEY, setSelectedYKey
 };
