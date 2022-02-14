a68c1e10bb49a0ac002ea29a8e81b9cda2821f1b
davekennewell <davekennewell@gmail.com>
2022-02-14 09:37:00 +0400

    prevent undefined error

--- geonode_mapstore_client/client/js/plugins/hydrata/Anuga/epicsAnuga.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Anuga/epicsAnuga.js
@@ -154,7 +154,7 @@ export const pollAnugaScenarioEpic = (action$, store) =>
                             let scenariosToLoadResults = action.scenarios?.filter(scenario => scenario.latest_run?.status === 'complete');
                             console.log('backend scenariosToLoadResults', scenariosToLoadResults);
                             // now swap to frontend
-                            scenariosToLoadResults = store.getState()?.anuga?.scenarios?.filter(scenario => scenario.id === scenariosToLoadResults[0].id);
+                            scenariosToLoadResults = store.getState()?.anuga?.scenarios?.filter(scenario => scenario.id === scenariosToLoadResults?.[0].id);
                             console.log('frontend scenariosToLoadResults', scenariosToLoadResults);
                             // and check frontend
                             if (scenariosToLoadResults?.length > 0 && !scenariosToLoadResults[0].isLoaded) {
