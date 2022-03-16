f1d4a586e591c245824227c6e910c257239a831c
davekennewell <davekennewell@gmail.com>
2022-03-16 15:07:04 +0400

    make sure scenario polling updates when a run starts

--- geonode_mapstore_client/client/js/plugins/hydrata/Anuga/reducersAnuga.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Anuga/reducersAnuga.js
@@ -57,6 +57,7 @@ export default ( state = initialState, action) => {
                 const newScenario = action.scenarios.filter(actionScenario => scenario.id === actionScenario.id)[0];
                 scenario.latest_run = newScenario?.latest_run || {status: 'unsaved'};
                 scenario.status = newScenario?.status || 'unsaved';
+                scenario.latest_run_is_valid = newScenario?.latest_run_is_valid;
                 return scenario;
             })
         };
