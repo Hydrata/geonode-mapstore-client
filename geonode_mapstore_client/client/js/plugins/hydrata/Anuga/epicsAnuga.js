a86587d87deda6724c514a2891f7a8ce2e65347b
davekennewell <davekennewell@gmail.com>
2022-06-03 15:27:17 +0400

    try to remove duplicate layers at elevation import

--- geonode_mapstore_client/client/js/plugins/hydrata/Anuga/epicsAnuga.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Anuga/epicsAnuga.js
@@ -140,11 +140,11 @@ export const pollAnugaElevationEpic = (action$, store) =>
                         )),
                         Rx.Observable.of(updateUploadStatus('Complete')),
                         Rx.Observable.of(saveDirectContent()),
-                        Rx.Observable.of(initAnuga()),
-                        Rx.Observable.of(() => {
-                            let wmsLayers = store.getState()?.layers?.flat?.filter((l) => l.type === 'wms' && l.group !== 'background') || [];
-                            return refreshLayers(wmsLayers);
-                        })
+                        Rx.Observable.of(initAnuga())
+                        // Rx.Observable.of(() => {
+                        //     let wmsLayers = store.getState()?.layers?.flat?.filter((l) => l.type === 'wms' && l.group !== 'background') || [];
+                        //     return refreshLayers(wmsLayers);
+                        // })
                     );
                 })
         );
