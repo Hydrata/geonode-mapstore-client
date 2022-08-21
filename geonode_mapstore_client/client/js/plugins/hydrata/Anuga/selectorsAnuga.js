da5152a89f85ce22df9120b4099f692533e0d0eb
davekennewell <davekennewell@gmail.com>
2022-08-21 14:44:05 +1000

    fixed perms on backend not frontend

--- geonode_mapstore_client/client/js/plugins/hydrata/Anuga/selectorsAnuga.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Anuga/selectorsAnuga.js
@@ -2,19 +2,19 @@
 
 export const canEditAnugaMap = (state) => {
     const currentUserId = state?.security?.user?.pk;
-    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => parseInt(user?.info?.sub, 10) === currentUserId)[0]?.permissions;
+    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => user.id === currentUserId)[0]?.permissions;
     return ["edit", "manage", "owner"].includes(currentUserPerm);
 };
 
 export const canManageAnugaMap = (state) => {
     const currentUserId = state?.security?.user?.pk;
-    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => parseInt(user?.info?.sub, 10) === currentUserId)[0]?.permissions;
+    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => user.id === currentUserId)[0]?.permissions;
     return ["manage", "owner"].includes(currentUserPerm);
 };
 
 export const isOwnerAnugaMap = (state) => {
     const currentUserId = state?.security?.user?.pk;
-    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => parseInt(user?.info?.sub, 10) === currentUserId)[0]?.permissions;
+    const currentUserPerm = state?.gnresource?.compactPermissions?.users?.filter(user => user.id === currentUserId)[0]?.permissions;
     return ["owner"].includes(currentUserPerm);
 };
 
