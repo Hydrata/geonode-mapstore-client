9e5bcca1fbfb30cc82ec2d125eec03b72d5f6e22
davekennewell <davekennewell@gmail.com>
2022-09-02 05:46:00 +0400

    use new feature formate for SwampID

--- geonode_mapstore_client/client/js/plugins/hydrata/Swamps/epicsSwamps.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Swamps/epicsSwamps.js
@@ -68,7 +68,7 @@ export const queryLayerAttributesToStoreStep1 = (action$, store) =>
         })
         .mergeMap((action) => Rx.Observable.merge(
             Rx.Observable.of(setVisibleSwampsChart(true)),
-            Rx.Observable.of(setSelectedSwampId(action?.data?.substring(action?.data.indexOf("FID_1 = ") + 8, action?.data.lastIndexOf('\nSWAMP')))),
+            Rx.Observable.of(setSelectedSwampId(`bluemountains_thpss_e_4480_32756.${action?.data?.substring(action?.data.indexOf("FID_1 = ") + 6, action?.data.lastIndexOf('\nSWAMP'))}`)),
             Rx.Observable.of(closeIdentify()),
             Rx.Observable.of(setLayer(action?.layer.id)),
             Rx.Observable.of(featureTypeSelected(store.getState()?.gnsettings?.geoserverUrl + '/wfs', action?.layer?.name))
