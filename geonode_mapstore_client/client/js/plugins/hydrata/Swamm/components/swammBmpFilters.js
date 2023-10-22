11759f3516c1f3079751e542e3486c5bf6a5f17d
davekennewell <davekennewell@gmail.com>
2023-10-22 16:44:48 +0400

    WIP BMP filters

--- geonode_mapstore_client/client/js/plugins/hydrata/Swamm/components/swammBmpFilters.js
+++ geonode_mapstore_client/client/js/plugins/hydrata/Swamm/components/swammBmpFilters.js
@@ -8,6 +8,7 @@ import {
     toggleBmpGroupProfileVisibility,
     toggleBmpStatusVisibility,
     setAllBmpTypesVisibility,
+    setExpandedBmpTypeGroupName,
     toggleBmpPriorityVisibility
 } from "../actionsSwamm";
 import {changeLayerProperties} from "../../../../../MapStore2/web/client/actions/layers";
@@ -33,8 +34,11 @@ class SwammBmpFiltersClass extends React.Component {
         bmpFootprintLayer: PropTypes.object,
         bmpOutletLayer: PropTypes.object,
         priorities: PropTypes.object,
+        statuses: PropTypes.object,
         groupProfiles: PropTypes.object,
-        statuses: PropTypes.object
+        bmpTypeGroups: PropTypes.array,
+        expandedBmpTypeGroupName: PropTypes.string,
+        setExpandedBmpTypeGroupName: PropTypes.func
     };
 
     static defaultProps = {}
@@ -62,43 +66,114 @@ class SwammBmpFiltersClass extends React.Component {
                                 onClick={() => {this.props.setExpandedFilter('bmpType');}}
                             />
                             <span className="inline h5 menu-row-text"><strong>Filter by BMP type</strong></span>
-                            {
-                                this.props.expandedFilter === "bmpType" ?
-                                    <React.Fragment>
-                                        <span
-                                            className={"inline btn glyphicon menu-row-glyph glyphicon-check"}
-                                            style={{"color": "limegreen", marginLeft: "30px"}}
-                                            onClick={() => {
-                                                this.props.setAllBmpTypesVisibility(true);
-                                            }}
-                                        />
-                                        <span className="inline h5 menu-row-text">Select All</span>
-                                        <span
-                                            className={"inline btn glyphicon menu-row-glyph glyphicon-unchecked"}
-                                            style={{"color": "red", marginLeft: "30px"}}
-                                            onClick={() => {
-                                                this.props.setAllBmpTypesVisibility(false);
-                                            }}
-                                        />
-                                        <span className="inline h5 menu-row-text">Select None</span>
-                                        <hr style={{margin: "0"}}/>
-                                        <Row>
-                                            {
-                                                this.props.bmpTypes.map((bmpType, index) => (
-                                                    <Col sm={6} className={"row menu-row filter-row " + (index % 2 ? "filter-row-odd" : '')}>
-                                                        <span
-                                                            className={"btn glyphicon menu-row-glyph " + (bmpType?.visibility ? "glyphicon-ok" : "glyphicon-remove")}
-                                                            style={{"color": bmpType?.visibility ? "limegreen" : "red"}}
-                                                            onClick={() => this.props.toggleBmpTypeVisibility(bmpType)}
-                                                        />
-                                                        <span className="menu-row-text">{bmpType?.name}</span>
-                                                    </Col>
-                                                ))
-                                            }
-                                        </Row>
-                                    </React.Fragment>
-                                    : null
+                            { this.props.expandedFilter === "bmpType" ?
+                                <React.Fragment>
+                                    {this.props.bmpTypeGroups?.map((group) => {
+                                        return (
+                                            <div
+                                                key={`group-${group}`}
+                                                style={{
+                                                    textAlign: "left",
+                                                    marginLeft: 0,
+                                                    marginBottom: "3px",
+                                                    padding: "3px",
+                                                    border: "1px solid white",
+                                                    borderRadius: "3px"
+                                                }}
+                                            >
+                                                <span
+                                                    style={{marginLeft: "15px"}}
+                                                    className={"btn glyphicon bmp-type-group-glyph" + (this.props.expandedBmpTypeGroupName === group[0] ? " glyphicon-chevron-down bmp-type-group-bottom-margin" : " glyphicon-chevron-right")}
+                                                    onClick={
+                                                        this.props.expandedBmpTypeGroupName === group[0] ?
+                                                            () => this.props.setExpandedBmpTypeGroupName(null) :
+                                                            () => this.props.setExpandedBmpTypeGroupName(group[0])
+                                                    }
+                                                />
+                                                <span className="bmp-type-group-name">
+                                                    {group[1]}
+                                                </span>
+                                                <span
+                                                    className={"inline btn glyphicon menu-row-glyph glyphicon-unchecked"}
+                                                    style={{"color": "red", marginLeft: "30px"}}
+                                                    onClick={() => {
+                                                        this.props.setGroupBmpTypesVisibility(expandedBmpTypeGroupName);
+                                                    }}
+                                                />
+                                                {
+                                                    this.props.expandedBmpTypeGroupName === group[0] ?
+                                                        this.props.bmpTypes
+                                                            .filter(bmpType => bmpType.group_name === group[0])
+                                                            .map(bmpType => {
+                                                                return (
+                                                                    <div
+                                                                        key={`bmpType-${bmpType?.name}`}
+                                                                        style={{
+                                                                            marginLeft: "30px"
+                                                                        }}
+                                                                    >
+                                                                        <input
+                                                                            id={`bmp-type-selector-box-${bmpType?.name}`}
+                                                                            // style={formControlStyle}
+                                                                            type={'radio'}
+                                                                            name={'bmpType'}
+                                                                            value={bmpType?.name}
+                                                                            onChange={this.handleBmpChange}
+                                                                        />
+                                                                        <label
+                                                                            htmlFor={`bmp-type-selector-box-${bmpType?.name}`}
+                                                                            style={{marginLeft: "6px", verticalAlign: "middle"}}
+                                                                        >
+                                                                            {bmpType?.name}
+                                                                        </label>
+                                                                    </div>
+                                                                );
+                                                            })
+                                                        : null
+                                                }
+                                            </div>
+                                        );
+                                    })}
+                                </React.Fragment>
+                                : null
                             }
+                            {/* {*/}
+                            {/*    this.props.expandedFilter === "bmpType" ?*/}
+                            {/*        <React.Fragment>*/}
+                            {/*            <span*/}
+                            {/*                className={"inline btn glyphicon menu-row-glyph glyphicon-check"}*/}
+                            {/*                style={{"color": "limegreen", marginLeft: "30px"}}*/}
+                            {/*                onClick={() => {*/}
+                            {/*                    this.props.setAllBmpTypesVisibility(true);*/}
+                            {/*                }}*/}
+                            {/*            />*/}
+                            {/*            <span className="inline h5 menu-row-text">Select All</span>*/}
+                            {/*            <span*/}
+                            {/*                className={"inline btn glyphicon menu-row-glyph glyphicon-unchecked"}*/}
+                            {/*                style={{"color": "red", marginLeft: "30px"}}*/}
+                            {/*                onClick={() => {*/}
+                            {/*                    this.props.setAllBmpTypesVisibility(false);*/}
+                            {/*                }}*/}
+                            {/*            />*/}
+                            {/*            <span className="inline h5 menu-row-text">Select None</span>*/}
+                            {/*            <hr style={{margin: "0"}}/>*/}
+                            {/*            <Row>*/}
+                            {/*                {*/}
+                            {/*                    this.props.bmpTypes.map((bmpType, index) => (*/}
+                            {/*                        <Col sm={6} className={"row menu-row filter-row " + (index % 2 ? "filter-row-odd" : '')}>*/}
+                            {/*                            <span*/}
+                            {/*                                className={"btn glyphicon menu-row-glyph " + (bmpType?.visibility ? "glyphicon-ok" : "glyphicon-remove")}*/}
+                            {/*                                style={{"color": bmpType?.visibility ? "limegreen" : "red"}}*/}
+                            {/*                                onClick={() => this.props.toggleBmpTypeVisibility(bmpType)}*/}
+                            {/*                            />*/}
+                            {/*                            <span className="menu-row-text">{bmpType?.name}</span>*/}
+                            {/*                        </Col>*/}
+                            {/*                    ))*/}
+                            {/*                }*/}
+                            {/*            </Row>*/}
+                            {/*        </React.Fragment>*/}
+                            {/*        : null*/}
+                            {/*}*/}
                         </div>
                         <div className={"row menu-row pull-left"} style={{width: "480px", textAlign: "left"}}>
                             <span
@@ -208,7 +283,6 @@ const mapStateToProps = (state) => {
     return {
         mapId: state?.swamm?.data?.base_map,
         projectData: state?.swamm?.data,
-        bmpTypes: state?.swamm?.bmpTypes,
         statuses: state?.swamm?.statuses,
         groupProfiles: viewableGroupProfiles,
         layers: state?.layers,
@@ -218,7 +292,10 @@ const mapStateToProps = (state) => {
         bmpOutletLayer: bmpOutletLayerSelector(state),
         bmpFootprintLayer: bmpFootprintLayerSelector(state),
         bmpWatershedLayer: bmpWatershedLayerSelector(state),
-        priorities: state?.swamm?.priorities
+        priorities: state?.swamm?.priorities,
+        bmpTypes: state?.swamm?.bmpTypes,
+        bmpTypeGroups: state?.swamm?.bmpTypeGroups || [],
+        expandedBmpTypeGroupName: state?.swamm?.expandedBmpTypeGroupName
     };
 };
 
@@ -230,7 +307,8 @@ const mapDispatchToProps = ( dispatch ) => {
         toggleBmpGroupProfileVisibility: (groupProfile) => dispatch(toggleBmpGroupProfileVisibility(groupProfile)),
         toggleBmpStatusVisibility: (status) => dispatch(toggleBmpStatusVisibility(status)),
         setAllBmpTypesVisibility: (boolValue) => dispatch(setAllBmpTypesVisibility(boolValue)),
-        toggleBmpPriorityVisibility: (priority) => dispatch(toggleBmpPriorityVisibility(priority))
+        toggleBmpPriorityVisibility: (priority) => dispatch(toggleBmpPriorityVisibility(priority)),
+        setExpandedBmpTypeGroupName: (expandedBmpTypeGroupName) => dispatch(setExpandedBmpTypeGroupName(expandedBmpTypeGroupName))
     };
 };
 
