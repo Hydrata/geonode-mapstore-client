8979b4e7b5ea87d14d465aad997a4fde5d91dccc
Dave Kennewell <david.kennewell@hydrata.com>
2024-01-15 09:04:01 +0400

    compile client

--- hydrata-build.sh
+++ hydrata-build.sh
@@ -1,4 +1,5 @@
 echo "Building geonode-mapstore-client"
+export NODE_OPTIONS=--openssl-legacy-provider
 npm run compile --prefix ./geonode_mapstore_client/client/
 git add ./geonode_mapstore_client/static/
 git commit -m "compile client" ./
