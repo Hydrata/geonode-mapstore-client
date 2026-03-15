a69f2343f214cc50184a6ea31b2d35070c289e14
Dave Kennewell <david.kennewell@hydrata.com>
2026-03-15 17:00:58 +0400

    fix: remove beforeunload dialog and move toast position to top
    Disable PendingStatePrompt (beforeunload "Changes may not be saved"
    dialog) in withPrompt.jsx. Move toast notifications from 10vh to 20px
    from top of page.

--- geonode_mapstore_client/client/js/plugins/save/withPrompt.jsx
+++ geonode_mapstore_client/client/js/plugins/save/withPrompt.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import PendingStatePrompt from '@mapstore/framework/plugins/ResourcesCatalog/containers/PendingStatePrompt';
-
 export default (Component) => {
     const PromptComponent = (props) => {
         return props.enabled
-            ? <>
-                <Component {...props}/>
-                <PendingStatePrompt
-                    show={props.show}
-                    onCancel={props.onCancel}
-                    onConfirm={props.onConfirm}
-                    pendingState={!!props.dirtyState}
-                    titleId="resourcesCatalog.detailsPendingChangesTitle"
-                    descriptionId="resourcesCatalog.detailsPendingChangesDescription"
-                    cancelId="resourcesCatalog.detailsPendingChangesCancel"
-                    confirmId="resourcesCatalog.detailsPendingChangesConfirm"
-                    variant="danger"
-                />
-            </>
+            ? <Component {...props}/>
             : null
         ;
     };
