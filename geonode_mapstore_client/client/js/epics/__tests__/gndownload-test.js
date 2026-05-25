33a2a9ff977c4fa604816bbfd16a13d22e4b4d8a
Dave Kennewell <david.kennewell@hydrata.com>
2026-05-25 11:13:40 +0400

    test(epics): mock axios in gnDownloadMetaData test to fix CI timeout flake

--- geonode_mapstore_client/client/js/epics/__tests__/gndownload-test.js
+++ geonode_mapstore_client/client/js/epics/__tests__/gndownload-test.js
@@ -7,21 +7,28 @@
  */
 
 import expect from 'expect';
+import MockAdapter from 'axios-mock-adapter';
+import axios from '@mapstore/framework/libs/ajax';
 import { testEpic } from '@mapstore/framework/epics/__tests__/epicTestUtils';
 import { downloadMetaData, DOWNLOAD_METADATA_COMPLETE } from '@js/actions/gndownload';
 import { gnDownloadMetaData } from '@js/epics/gndownload';
 
+let mockAxios;
+
 describe('gnDownloadMetaData epic', () => {
     beforeEach(done => {
         global.__DEVTOOLS__ = true;
+        mockAxios = new MockAdapter(axios);
         setTimeout(done);
     });
     afterEach(done => {
         delete global.__DEVTOOLS__;
+        mockAxios.restore();
         setTimeout(done);
     });
     it('should download metadata', (done) => {
         const NUM_ACTIONS = 1;
+        mockAxios.onGet(/iso_metadata_xml/).reply(200, '<gmd:MD_Metadata/>', { 'content-type': 'application/octet-stream' });
 
         testEpic(
             gnDownloadMetaData,
